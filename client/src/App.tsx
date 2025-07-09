b724d31d185abe26d29a601213d8c7c937ce13a0
Salabombou <salabombo@outlook.com>
2025-07-09 15:13:13 +0000

    stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -633,7 +633,6 @@ export default function App() {
             setSecret={setSecretUnlocked}
           />
           <Queue
-            key={queueList.length}
             tracks={tracks}
             queueList={queueList}
             currentTrack={currentTrack}
